7a5597c7b973c7692abdac2179bdb1ffd373eefb
Charlie Root <Humanoid-Human@users.noreply.github.com>
2026-03-13 14:40:22 -0400

    ports command

--- pkgdist.lua
+++ pkgdist.lua
@@ -2,17 +2,30 @@
 
 local ucl = require('ucl')
 
-local tar = function (input, output)
-	return 0 == os.execute('tar cJf '..output..' '..input)
+local tar_xz = 'tar -J --options xz:threads=0'
+
+local src = function ()
+	local exclude = '--exclude .svn --exclude .zfs --exclude .git --exclude @ --exclude release/dist --exclude release/obj'
+	return 0 == os.execute(tar_xz..' -cvLf src.txz '..exclude..' /usr/src')
+end
+
+local tests = function ()
+	return 0 == os.execute(tar_xz..' -cvLf tests.txz /usr/tests')
+end
+
+local ports = function ()
+	local exclude = '--exclude .svn --exclude .git --exclude distfiles --exclude packages --exclude INDEX* --exclude work'
+	return 0 == os.execute(tar_xz..' -cvLf ports.txz '..exclude..' /usr/ports')
 end
 
 assert(#arg == 1)
 
 if arg[1] == 'src' then
-	tar('/usr/src', 'src.txz')
+	src()
 elseif arg[1] == 'tests' then
-	tar('/usr/tests', 'tests.txz')
+	tests()
+elseif arg[1] == 'ports' then
+	ports()	
 else
 	error('Unknown argument '..arg[1])
 end
-
