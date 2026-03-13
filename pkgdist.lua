58b7b8f5cf3b19f83c662e8a6b904e7f50365c47
Charlie Root <Humanoid-Human@users.noreply.github.com>
2026-03-13 11:25:33 -0400

    pkgdist.lua: src and tests commands

--- pkgdist.lua
+++ pkgdist.lua
@@ -2,4 +2,17 @@
 
 local ucl = require('ucl')
 
+local tar = function (input, output)
+	return 0 == os.execute('tar cJf '..output..' '..input)
+end
+
+assert(#arg == 1)
+
+if arg[1] == 'src' then
+	tar('/usr/src', 'src.txz')
+elseif arg[1] == 'tests' then
+	tar('/usr/tests', 'tests.txz')
+else
+	error('Unknown argument '..arg[1])
+end
 
