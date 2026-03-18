2c3825fff59cd9e584e1557e1315f80d7c883581
Humanoid-Human <Humanoid-Human@users.noreply.github.com>
2026-03-18 16:29:05 -0400

    minor bug fixes

--- pkgdist.lua
+++ pkgdist.lua
@@ -2,55 +2,55 @@
 
 local ucl = require('ucl')
 
-local tar_xz = 'XZ_OPTS=--threads=0 tar -cvJf'
+local tar_xz = 'XZ_OPTS=--threads=0 tar -cvJf '
 
 local repeat_opt = function (opt, table)
-	local s = ''
-	for _, v in ipairs(exclude) do
-		s = s..' '..opt..' '..v
+	local s = ' '
+	for _, v in ipairs(table) do
+		s = s..opt..' '..v..' '
 	end
 	return s
 end
 
 local src = function (path)
-	if path == nil then path = ' /usr/src' end
+	if path == nil then path = '/usr/src' end
 	local exclude = {'.svn', '.zfs', '.git', '@', 'release/dist', 'release/obj'}
-	local cmd = repeat_opt('--exclude', exclude)
-	return 0 == os.execute(tar_xz..' -L src.txz'..cmd..path)
+	local rep = repeat_opt('--exclude', exclude)
+	return 0 == os.execute(tar_xz..'-L src.txz'..rep..path)
 end
 
 local tests = function (path)
-	if path == nil then path = ' /usr/tests' end
-	return 0 == os.execute(tar_xz..' -L tests.txz'..path)
+	if path == nil then path = '/usr/tests' end
+	return 0 == os.execute(tar_xz..'-L tests.txz '..path)
 end
 
 local ports = function (path)
-	if path == nil then path = ' /usr/ports' end
+	if path == nil then path = '/usr/ports' end
 	local exclude = {'.svn', '.git', 'distfiles', 'packages', 'INDEX*', 'work'}
-	local cmd = repeat_opt('--exclude', exclude)
-	return 0 == os.execute(tar_xz..' -L ports.txz'..cmd..path)
+	local rep = repeat_opt('--exclude', exclude)
+	return 0 == os.execute(tar_xz..'-L ports.txz'..cmd..path)
 end
 
 local kernel = function (path)
-	if path == nil then path = ' /boot/kernel' end
-	return 0 == os.execute(tar_xz..' kernel.txz --exclude *.debug '..path)
+	if path == nil then path = '/boot/kernel' end
+	return 0 == os.execute(tar_xz..'kernel.txz --exclude *.debug '..path)
 end
 
 local base = function (path)
-	if path == nil then path = ' /' end
+	if path == nil then path = '/' end
 	local include = {
-		'COPYRIGHT', 'bin', 'boot', 'dev', 'etc', 'lib', 'libexec', 'media',
-		'mnt', 'net', 'proc', 'rescue', 'root', 'sbin', 'tmp', 'usr', 'var'
+		'/COPYRIGHT', '/bin', '/boot', '/dev', '/etc', '/lib', '/libexec', '/media',
+		'/mnt', '/net', '/proc', '/rescue', '/root', '/sbin', '/tmp', '/usr', '/var'
 	}
 	local exclude = {
-		'dev/*', 'dev/.*', 'media/*', 'media/.*', 'mnt/*', 'mnt/.*', 'net/*',
-		'net/.*', 'proc/*', 'proc/.*', 'tmp/*', 'tmp/.*', 'usr/src/*',
-		'usr/src/.*', 'usr/obj/*', 'usr/obj/.*','usr/lib32/*', 'usr/lib32/.*',
-		'usr/local/*', 'usr/local/.*', 'var/[!y]*/*', 'var/y[!p]*/*' -- don't exclude contents of /var/yp 
+		'/dev/*', '/dev/.*', '/media/*', '/media/.*', '/mnt/*', '/mnt/.*', '/net/*',
+		'/net/.*', '/proc/*', '/proc/.*', '/tmp/*', '/tmp/.*', '/usr/src/*',
+		'/usr/src/.*', '/usr/obj/*', '/usr/obj/.*','/usr/lib32/*', '/usr/lib32/.*',
+		'/usr/local/*', '/usr/local/.*', '/var/[!y]*/*', '/var/y[!p]*/*' -- don't exclude contents of /var/yp 
 	}
 	local inc_opts = repeat_opt('--include', include)
 	local exc_opts = repeat_opt('--exclude', exclude)
-	return 0 == os.execute(tar_xz..' base.txz'..inc_opts..exc_opts..path)
+	return 0 == os.execute(tar_xz..'base.txz'..inc_opts..exc_opts..path)
 end
 
 assert(#arg == 1 or #arg == 2, 'Expected 1-2 arguments, got '..tostring(#arg))
